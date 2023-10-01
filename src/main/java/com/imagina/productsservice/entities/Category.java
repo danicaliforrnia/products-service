6d3328e33fa20eef3aa9c8354899855037f28eb4
stefanellid <stefanellid@media-saturn.com>
2023-10-01 14:14:48 +0200

    feat: extend base entity and provide override methods

--- src/main/java/com/imagina/productsservice/entities/Category.java
+++ src/main/java/com/imagina/productsservice/entities/Category.java
@@ -1,21 +1,18 @@
 package com.imagina.productsservice.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name = "category")
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Category {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class Category extends BaseEntity {
 
     @Column(name = "name")
     private String name;
@@ -25,5 +22,28 @@ public class Category {
 
     @OneToMany(mappedBy = "category")
     private List<Product> products;
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj) {
+            return true;
+        }
+
+        if(!(obj instanceof Category product)) {
+            return false;
+        }
+
+        return Objects.equals(name, product.getName());
+    }
+
+    @Override
+    public String toString() {
+        return "Category ID: " + super.getId() + ", name: " + name;
+    }
 
 }
--- src/main/java/com/imagina/productsservice/entities/Product.java
+++ src/main/java/com/imagina/productsservice/entities/Product.java
@@ -1,22 +1,22 @@
 package com.imagina.productsservice.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+
+import java.util.Objects;
 
 @Entity
 @Table(name = "product")
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Product {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
+public class Product extends BaseEntity {
     @Column(name = "name")
     private String name;
+
+    @Column(name = "code")
+    private Integer code;
 
     @Column(name = "description")
     private String description;
@@ -30,4 +30,27 @@ public class Product {
 
     @OneToOne(mappedBy = "product", fetch = FetchType.LAZY)
     private ProductDetails productDetails;
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(code);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj) {
+            return true;
+        }
+
+        if(!(obj instanceof Product product)) {
+            return false;
+        }
+
+        return Objects.equals(code, product.getCode());
+    }
+
+    @Override
+    public String toString() {
+        return "product ID: " + super.getId() + ", name: " + name;
+    }
 }
