9d31547b8f25046f86502df71dcc07ea1d82d85e
Daniel Stefanelli <daniel.stefanelli.h@gmail.com>
2023-10-23 21:18:37 +0200

    feat: add unit field to import.sql and DTOs

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,9 +1,9 @@
 INSERT INTO category (name, description, created_by, modified_by, created_date, modified_date) VALUES ('Electrónica', 'Productos de electrónica', 'admin', 'admin', now(), now());
 INSERT INTO category (name, description, created_by, modified_by, created_date, modified_date) VALUES ('Telefonía', 'Productos de telefonía',  'admin', 'admin', now(), now());
 
-INSERT INTO product (name, code, description, price, category_id, created_by, modified_by, created_date, modified_date) SELECT 'Nintendo Switch OLED', 1000, 'Nintendo Switch OLED Edition', 300, id, 'admin', 'admin', now(), now() FROM category WHERE name = 'Electrónica';
-INSERT INTO product (name, code, description, price, category_id, created_by, modified_by, created_date, modified_date) SELECT 'Playstation 5', 1001, 'PS5 Ultimate Edition', 400, id, 'admin', 'admin', now(), now() FROM category WHERE name = 'Electrónica';
-INSERT INTO product (name, code, description, price, category_id, created_by, modified_by, created_date, modified_date) SELECT 'iPhone 15 pro MAX', 1002, 'iPhone 14 1TB', 1500, id, 'admin', 'admin', now(), now() FROM category WHERE name = 'Telefonía';
+INSERT INTO product (name, code, description, units, price, category_id, created_by, modified_by, created_date, modified_date) SELECT 'Nintendo Switch OLED', 1000, 'Nintendo Switch OLED Edition', 100, 300, id, 'admin', 'admin', now(), now() FROM category WHERE name = 'Electrónica';
+INSERT INTO product (name, code, description, units, price, category_id, created_by, modified_by, created_date, modified_date) SELECT 'Playstation 5', 1001, 'PS5 Ultimate Edition', 200, 400, id, 'admin', 'admin', now(), now() FROM category WHERE name = 'Electrónica';
+INSERT INTO product (name, code, description, units, price, category_id, created_by, modified_by, created_date, modified_date) SELECT 'iPhone 15 pro MAX', 1002, 'iPhone 14 1TB', 150, 1500, id, 'admin', 'admin', now(), now() FROM category WHERE name = 'Telefonía';
 
 
 INSERT INTO product_details (color, height, weight, product_id) SELECT 'black', 30, 100, id FROM product WHERE name = 'Nintendo Switch OLED';
