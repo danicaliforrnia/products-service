eb67513deaecb722a29e4054f8290e087b97389e
Daniel Stefanelli <daniel.stefanelli.h@gmail.com>
2023-10-23 21:18:37 +0200

    feat: extend base entity and provide override methods

--- src/main/java/com/imagina/productsservice/entities/ProductDetails.java
+++ src/main/java/com/imagina/productsservice/entities/ProductDetails.java
@@ -1,13 +1,14 @@
 package com.imagina.productsservice.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+
+import java.util.Objects;
 
 @Entity
 @Table(name = "product_details")
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProductDetails {
@@ -27,4 +28,27 @@ public class ProductDetails {
     @MapsId
     @JoinColumn(name = "product_id", referencedColumnName = "id", nullable = false)
     private Product product;
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(product.getCode());
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj) {
+            return true;
+        }
+
+        if(!(obj instanceof ProductDetails productDetails)) {
+            return false;
+        }
+
+        return Objects.equals(product.getCode(), productDetails.getProduct().getCode());
+    }
+
+    @Override
+    public String toString() {
+        return "product ID: " + id;
+    }
 }
