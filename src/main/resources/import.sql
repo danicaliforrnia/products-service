54e6d5d62e5cf44add0bc879ede2a277f60ca60e
Daniel Stefanelli <daniel.stefanelli.h@gmail.com>
2023-10-23 21:18:36 +0200

    feat: add jpa and postgres dependency and use code-first approach

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO product (name, description, price) VALUES ('Nintendo Switch OLED', 'Nitendo Switch OLED Edition', 300);
+INSERT INTO product (name, description, price) VALUES ('Playstation 5', 'PS5 Ultimate Edition', 400);
+INSERT INTO product (name, description, price) VALUES ('iPhone 15 pro MAX', 'iPhone 14 1TB', 1500);
