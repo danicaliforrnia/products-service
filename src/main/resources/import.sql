636a5225602509843d0fb0da7e87990e4f263fca
Daniel Stefanelli <daniel.stefanelli.h@gmail.com>
2023-10-23 21:18:37 +0200

    feat: add audit feature

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,9 +1,9 @@
-INSERT INTO category (name, description) VALUES ('Electrónica', 'Productos de electrónica');
-INSERT INTO category (name, description) VALUES ('Telefonía', 'Productos de telefonía');
+INSERT INTO category (name, description, created_by, modified_by, created_date, modified_date) VALUES ('Electrónica', 'Productos de electrónica', 'admin', 'admin', now(), now());
+INSERT INTO category (name, description, created_by, modified_by, created_date, modified_date) VALUES ('Telefonía', 'Productos de telefonía',  'admin', 'admin', now(), now());
 
-INSERT INTO product (name, description, price, category_id) SELECT 'Nintendo Switch OLED', 'Nintendo Switch OLED Edition', 300, id FROM category WHERE name = 'Electrónica';
-INSERT INTO product (name, description, price, category_id) SELECT 'Playstation 5', 'PS5 Ultimate Edition', 400, id FROM category WHERE name = 'Electrónica';
-INSERT INTO product (name, description, price, category_id) SELECT 'iPhone 15 pro MAX', 'iPhone 14 1TB', 1500, id FROM category WHERE name = 'Telefonía';
+INSERT INTO product (name, code, description, price, category_id, created_by, modified_by, created_date, modified_date) SELECT 'Nintendo Switch OLED', 1000, 'Nintendo Switch OLED Edition', 300, id, 'admin', 'admin', now(), now() FROM category WHERE name = 'Electrónica';
+INSERT INTO product (name, code, description, price, category_id, created_by, modified_by, created_date, modified_date) SELECT 'Playstation 5', 1001, 'PS5 Ultimate Edition', 400, id, 'admin', 'admin', now(), now() FROM category WHERE name = 'Electrónica';
+INSERT INTO product (name, code, description, price, category_id, created_by, modified_by, created_date, modified_date) SELECT 'iPhone 15 pro MAX', 1002, 'iPhone 14 1TB', 1500, id, 'admin', 'admin', now(), now() FROM category WHERE name = 'Telefonía';
 
 
 INSERT INTO product_details (color, height, weight, product_id) SELECT 'black', 30, 100, id FROM product WHERE name = 'Nintendo Switch OLED';
