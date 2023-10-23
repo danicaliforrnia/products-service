782f06217b442dcabef0f47849e9703f742f27a2
Daniel Stefanelli <daniel.stefanelli.h@gmail.com>
2023-10-23 21:18:37 +0200

    feat: add caching

--- src/main/java/com/imagina/productsservice/controllers/ProductsController.java
+++ src/main/java/com/imagina/productsservice/controllers/ProductsController.java
@@ -4,6 +4,9 @@ import com.imagina.productsservice.dtos.InputProductDto;
 import com.imagina.productsservice.dtos.ReadProductDto;
 import com.imagina.productsservice.services.ProductsService;
 import jakarta.validation.Valid;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,6 +23,7 @@ public class ProductsController {
     }
 
     @GetMapping(produces = "application/json")
+    @Cacheable("products")
     public Page<ReadProductDto> findAll(@RequestParam(name = "name", required = false) String name,
                                         @RequestParam(name = "page", required = false, defaultValue = "0") String page,
                                         @RequestParam(name = "size", required = false, defaultValue = "5") String size) {
@@ -32,16 +36,19 @@ public class ProductsController {
     }
 
     @PostMapping(consumes = "application/json", produces = "application/json")
+    @CacheEvict(cacheNames = "products", allEntries = true)
     public ReadProductDto create(@Valid @RequestBody InputProductDto inputProductDto) {
         return productsService.create(inputProductDto);
     }
 
     @PutMapping(path = "/{id}", consumes = "application/json", produces = "application/json")
+    @CacheEvict(cacheNames = "products", allEntries = true)
     public ReadProductDto update(@PathVariable Long id, @Valid @RequestBody InputProductDto inputProductDto) {
         return productsService.update(id, inputProductDto);
     }
 
     @DeleteMapping("/{id}")
+    @CacheEvict(cacheNames = "products", allEntries = true)
     public void delete(@PathVariable Long id) {
         productsService.delete(id);
     }
