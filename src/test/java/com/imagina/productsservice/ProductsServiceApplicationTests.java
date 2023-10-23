196d2afd9fbe9092a35f94bf4b7176798e107e2c
Daniel Stefanelli <daniel.stefanelli.h@gmail.com>
2023-10-23 21:18:36 +0200

    first commit

---
+++ src/test/java/com/imagina/productsservice/ProductsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imagina.productsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
