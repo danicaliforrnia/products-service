0d2546bf0d39f7ef7c061da428f85057d53b0788
Daniel Stefanelli <daniel.stefanelli.h@gmail.com>
2023-10-23 21:18:37 +0200

    feat: fin by with containing

--- src/main/java/com/imagina/productsservice/repositories/ProductsRepository.java
+++ src/main/java/com/imagina/productsservice/repositories/ProductsRepository.java
@@ -8,5 +8,5 @@ import java.util.List;
 
 @Repository
 public interface ProductsRepository extends JpaRepository<Product, Long> {
-    List<Product> findByName(String name);
+    List<Product> findAllByNameContainingIgnoreCase(String name);
 }
