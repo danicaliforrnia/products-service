a2fe6786f95ad50947970280eb50065e97f5883c
Daniel Stefanelli <daniel.stefanelli.h@gmail.com>
2023-10-23 21:18:37 +0200

    feat: create cart and cart item entities

---
+++ src/main/java/com/imagina/productsservice/entities/CartItem.java
@@ -0,0 +1,54 @@
+package com.imagina.productsservice.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "cart_item")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartItem extends BaseEntity {
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "units")
+    private Integer units;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cart_id", nullable = false)
+    private Cart cart;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (!(obj instanceof CartItem cartItem)) {
+            return false;
+        }
+
+        return super.getId() != null && super.getId().equals(cartItem.getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Cart item for cart ID: " + getCart().getId() + "and product ID: " + product.getId();
+    }
+}
